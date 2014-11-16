e2b4e044e643ac796b345d01f20be58c4dc3a234
Mark Provenzano <mark@dreambound.us>
2014-11-15 19:23:23 -0500

    Added base classes
    This adds the bulk of the content needed for the class assignment. All
    that is left is comments.

---
+++ TriangleUnitTest/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleUnitTest {
+	public class Triangle {
+		public int SideA { get; set; }
+		public int SideB { get; set; }
+		public int SideC { get; set; }
+
+		public Triangle(int a , int b , int c) {
+			SideA = a;
+			SideB = b;
+			SideC = c;
+		}
+
+		public bool IsTriangle() {
+			return (SideA + SideB >= SideC) && (SideA + SideC >= SideB) && (SideB + SideC >= SideA);
+		}
+
+		public bool IsEquilateral() {
+			return SideA == SideB && SideB == SideC && SideC == SideA ? true : false; 
+		}
+
+		public bool IsIsosceles() {
+			var sides = new List<int>() { SideA , SideB , SideC };
+			var count = 0;
+
+			for(int i = 0 ; i < sides.Count ; i++ ) {
+				if(i < sides.Count - 1) {
+					count += sides[i] == sides[i + 1] ? 1 : 0;
+				}
+				else {
+					count += sides[i] == sides[i - i] ? 1 : 0;
+				}
+			}
+
+			return count == 1;
+		}
+
+		public bool IsScalene() {
+			return SideA != SideB && SideB != SideC && SideC != SideA ? true : false; 
+		}
+	}
+}
---
+++ Triangle_Test/Triangle_Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleUnitTest;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Triangle_Tests {
+	[TestClass]
+	public class Triangle_Test {
+
+		List<Triangle> TArray = new List<Triangle>() {
+			new Triangle(5 , 5 , 5) ,
+			new Triangle(2 , 3 , 2) ,
+			new Triangle(3 , 4 , 5) ,
+			new Triangle(4 , 2 , 1)
+		};
+
+		[TestMethod]
+		public void IsTriangle_Test() {
+			var expected = 3;
+			var actual = TArray.Where(a => a.IsTriangle()).Count();
+
+			Assert.AreEqual(expected , actual);
+		}
+
+		[TestMethod]
+		public void IsEquilateral_Test() {
+			var expected = 1;
+			if(TArray[0].IsEquilateral()) {
+				var actual = TArray.Where(a => a.IsEquilateral()).Count();
+				Assert.AreEqual(expected , actual);
+			}
+			else {
+				Assert.Fail();
+			}
+		}
+
+		[TestMethod]
+		public void IsIsosceles_Test() {
+			var expected = 1;
+			if(TArray[1].IsIsosceles()) {
+				var actual = TArray.Where(a => a.IsIsosceles() && a.IsTriangle()).Count();
+				Assert.AreEqual(expected , actual);
+			}
+			else {
+				Assert.Fail();
+			}
+		}
+
+		[TestMethod]
+		public void IsScalene_Test() {
+			var expected = 1;
+			if(TArray[2].IsScalene()) {
+				var actual = TArray.Where(a => a.IsScalene() && a.IsTriangle()).Count();
+				Assert.AreEqual(expected , actual);
+			}
+			else {
+				Assert.Fail();
+			}
+		}
+	}
+}
